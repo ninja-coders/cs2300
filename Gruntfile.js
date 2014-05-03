5196b33b4b7feaea18ba337d3737215305124dbf
Michael Wright <mkwright@gmail.com>
2014-05-03 00:18:17 -0600

    Have a simple setup that will compile jade to html and then deploy to site

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,47 @@
 var shell = require('shelljs');
 
+var buildDir = 'build';
+var siteDir = 'site';
+
 module.exports = function(grunt) {
   
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true, 
+          cwd: 'src/jade',
+          src: '*.jade',
+          dest: buildDir,
+          ext: '.html'
+        }]
+      }
+    },
+    assemble: {
+      options: {
+        assets: 'dist'
+      },
+      index: {
+        files: { 'site/index.html': [buildDir + '/index.html']},
+        options: { 
+          assets: 'dist',
+          layout: 'none'
+        }
+      }
+    }, 
+    clean: [buildDir + '/*', siteDir + '/**/*.html']
   });
 
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-newer');
-  grunt.registerTask('default', ['newer:assemble']);
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.registerTask('build', ['clean', 'jade:compile', 'newer:assemble']);
+  grunt.registerTask('default', ['build']);
 
   grunt.registerTask('sayHello', 'Say Hi!', function() {
     grunt.log.write('Hello!!!').ok();
@@ -16,7 +49,7 @@ module.exports = function(grunt) {
 
   grunt.registerTask('startServer', 'Start a static server', function() {
     var original =  process.cwd();
-    process.chdir('build');
+    process.chdir(siteDir);
     try {
       shell.exec('static-server');
     } catch (err) {
