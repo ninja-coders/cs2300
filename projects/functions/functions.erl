97821a66eeb81c3f943f328005a01eca7f2cd680
Michael Wright <mkwright@gmail.com>
2014-05-22 14:20:11 -0600

    Added sample functions usering erlang

---
+++ projects/functions/functions.erl
@@ -0,0 +1,22 @@
+-module(functions).
+-export([make_even/1, sum/1, word_length/1, count/1, average/2]).
+
+%% This is a map function
+make_even([]) -> [];
+make_even([H|T]) -> [H*2|make_even(T)].
+
+%% This is a map function
+word_length([]) -> [];
+word_length([H|T]) -> [length(H)|word_length(T)].
+
+%% This is a Reduce
+sum([]) -> 0;
+sum([H|T]) -> H + sum(T).
+
+%% This is a Reduce
+count([]) -> 0;
+count([_|T]) -> 1 + count(T).
+
+%% Demonstrate using functional composition
+average(T,C) -> T / C.
+
